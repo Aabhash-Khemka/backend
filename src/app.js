c42190b3611257752449cb97bf72d88580ed414b
Aabhash-Khemka <aabhashkhemka@gmail.com>
2025-07-29 16:42:26 +0530

    made controller ans routes for user

--- src/app.js
+++ src/app.js
@@ -11,4 +11,9 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+import userRouter from "/backend/src/routes/user.routes.js"
+// error 
+
+app.use("/api/v1/user",userRouter)
 export {app}
